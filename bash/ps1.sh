245058542101f2b51652d5b6667ae91a36e74b27
florian <florian@harfanglab.fr>
2022-03-27 14:40:33 +0200

    update ps1

--- bash/ps1.sh
+++ bash/ps1.sh
@@ -1,3 +1,3 @@
 [ -f ~/.kube-ps1/kube-ps1.sh ] && . ~/.kube-ps1/kube-ps1.sh
 
-PS1="${PS1:0:-3}\[\033[33m\] [\$(git symbolic-ref --short HEAD 2>/dev/null)] \$(kube_ps1 2>/dev/null) \[\033[00m\]\$ "
+PS1="\[\033[95m\]\u@\h \[\033[32m\]\w\[\033[33m\] [\$(git symbolic-ref --short HEAD 2>/dev/null)]\[\033[00m\] \$(kube_ps1 2>/dev/null)\n\$ "
